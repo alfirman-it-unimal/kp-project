b48bc90a12b859864985a48b3f3b2e5c8f4d21a8
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-01-04 23:09:25 +0700

    fix: script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,17 @@ import Layout from "@/components/layout";
 import Head from "next/head";
 import { Provider } from "react-redux";
 import store from "@/config/redux";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Layout>
         <Head>
-          <script
+          <Script
             src="https://kit.fontawesome.com/94e3743eb1.js"
             crossOrigin="anonymous"
-          ></script>
+          ></Script>
         </Head>
         <Component {...pageProps} />
       </Layout>
