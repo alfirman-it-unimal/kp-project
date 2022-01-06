61aedb378be8c21e5aee48af7fffbcaf1f3baead
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-01-06 12:23:57 +0700

    feat: add requirement page

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -16,6 +16,11 @@ export const Nav = () => {
           {!isLogin ? (
             <>
               <li>
+                <NavLink link="/requirement" activeClassname="active">
+                  Syarat
+                </NavLink>
+              </li>
+              <li>
                 <NavLink link="/register" activeClassname="active">
                   Registrasi
                 </NavLink>
---
+++ src/pages/requirement.tsx
@@ -0,0 +1,123 @@
+import { NextPage } from "next";
+
+const requirement: NextPage = () => {
+  return (
+    <div className="container-penduduk">
+      <h3>SYARAT</h3>
+      <div className="px-10 py-3">
+        <ol type="1" className="list-decimal space-y-4">
+          <li>
+            <p className="text-gray-700">Akta Kelahiran</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">Akta Kematian</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">KTP Elektronik</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">KTP Hilang</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">KTP Rusak</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">Surat Pindah</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">Surat Datang</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">Pembuatan KK Baru</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">Persyaratan Peubahan KK</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">Pembuatan KIA</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">KIA Perpanjang</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+          <li>
+            <p className="text-gray-700">KIA Hilang</p>
+            <ul className="list-disc px-5 text-gray-500">
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+              <li>Surat kelahiran dari dokter (asli)</li>
+            </ul>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default requirement;
